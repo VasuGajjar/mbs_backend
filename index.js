0830eda2ee75b95a7054c7bd6f80add1ccba5f4b
Vasu Gajjar <vasugajjar1@gmail.com>
2023-04-15 23:14:01 +0530

    added open api documentation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('./common/extensions');
 
 const express = require("express");
 const jwtValidator = require('./middlewares/jwt');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use('/api/medicine', jwtValidator, require("./modules/medicine/router"));
 app.use('/api/patient', jwtValidator, require("./modules/patient/router"));
 app.use('/api/prescription', jwtValidator, require("./modules/prescription/router"));
 app.use('/api/pharmacy/', jwtValidator, require("./modules/pharmacy/router"));
+//OpenApi Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(
     process.env.SERVER_PORT,
